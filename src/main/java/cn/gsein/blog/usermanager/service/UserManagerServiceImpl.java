1b2cd227fd8ee272a8dbadec8b10356cc59dca30
lhing17 <lhing17@163.com>
2017-04-21 08:30:53 +0800

    去掉通用Mapper，增加自写BaseMapper，增加UserMapper和测试类

---
+++ src/main/java/cn/gsein/blog/usermanager/service/UserManagerServiceImpl.java
@@ -0,0 +1,25 @@
+package cn.gsein.blog.usermanager.service;
+
+import cn.gsein.blog.base.entity.PageAndOrder;
+import cn.gsein.blog.usermanager.entity.User;
+import cn.gsein.blog.usermanager.mapper.UserMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by G.Seinfeld on 2017/4/20.
+ */
+@Service("userManagerService")
+public class UserManagerServiceImpl implements UserManagerService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    public List<User> findByPageAndOrder(String pageIndex, String orderField) {
+        int pageIndexInt = new Integer(pageIndex);
+        PageAndOrder pao = new PageAndOrder((pageIndexInt-1)*10, 10, orderField);
+        return userMapper.queryAllByPageAndOrder(pao);
+    }
+}
