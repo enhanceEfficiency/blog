40c14ae27c4db698f619b8c44e5bfb7ee555a77e
lhing17 <lhing17@163.com>
2017-08-31 13:40:41 +0800

    引入后台界面

---
+++ src/main/java/cn/gsein/blog/BlogApplication.java
@@ -0,0 +1,14 @@
+package cn.gsein.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+}
