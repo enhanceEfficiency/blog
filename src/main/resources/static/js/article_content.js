b4e7d91a2b6a2ad16baaf2cbb41bc41b5c86b4ab
lhing17 <lhing17@163.com>
2017-09-08 17:27:30 +0800

    完成部分页面内容替换

--- src/main/resources/static/js/article_content.js
+++ src/main/resources/static/js/article_content.js
@@ -1,12 +1,12 @@
 $(function () {
     editormd.markdownToHTML(
-        "blog_content", {
+        "article_content", {
             htmlDecode: "style,script,<iframe",  // you can filter tags decode
             emoji: true,
             taskList: true,
             tex: true,  // 默认不解析
             flowChart: true,  // 默认不解析
-            sequenceDiagram: true,  // 默认不解析 // 默认不解析
+            sequenceDiagram: true // 默认不解析 // 默认不解析
         }
     )
 });
